4f96a6e86f71ef1aa6fe3a906e478d133a65eba8
Arthur Martins Freire <mfarthurr@gmail.com>
2026-05-19 18:47:35 -0300

    initial commit

---
+++ ClassRoomPB/README.md
@@ -0,0 +1,5 @@
+# ClassRoomPB
+Sistema de controle acadêmico simplificado. Projeto desenvolvido para a disciplina de Engenharia de Software.
+
+## Estrutura
+Projeto estruturado em Java utilizando CLI e armazenamento local em arquivos.
---
+++ ClassRoomPB/src/main/java/br/edu/uepb/classroompb/Main.java
@@ -0,0 +1,8 @@
+package br.edu.uepb.classroompb;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Iniciando a CLI do ClassRoomPB...");
+        // Aqui você chamará a classe principal da View para iniciar o terminal
+    }
+}
