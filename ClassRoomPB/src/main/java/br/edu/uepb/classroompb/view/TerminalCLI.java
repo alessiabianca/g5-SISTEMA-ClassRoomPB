c84c7015f0ecce8392268a8a40bdb3866160074e
Keison Raniery <keison.filho@aluno.uepb.edu.br>
2026-05-21 09:19:54 -0300

    feat: adicionar suite de testes do periodo e finalizar comandos da US06

--- ClassRoomPB/src/main/java/br/edu/uepb/classroompb/view/TerminalCLI.java
+++ ClassRoomPB/src/main/java/br/edu/uepb/classroompb/view/TerminalCLI.java
@@ -1,13 +1,30 @@
+// src/main/java/br/edu/uepb/classroompb/view/TerminalCLI.java
 package br.edu.uepb.classroompb.view;
 
 import java.util.Scanner;
 
+import br.edu.uepb.classroompb.repository.PeriodoRepository;
+import br.edu.uepb.classroompb.repository.TurmaRepository;
+import br.edu.uepb.classroompb.service.PeriodoService; // ADICIONADO ESTE IMPORT
+import br.edu.uepb.classroompb.service.TurmaService;
+
 public class TerminalCLI {
     private final AuthCLI authCLI = new AuthCLI();
-    private final AdminCLI adminCLI = new AdminCLI();
     private final CoordenadorCLI coordenadorCLI = new CoordenadorCLI();
     private final ProfessorCLI professorCLI = new ProfessorCLI();
     private final AlunoCLI alunoCLI = new AlunoCLI();
+
+    // Instancie os repositórios e serviços necessários
+    PeriodoRepository periodoRepository = new PeriodoRepository();
+    TurmaRepository turmaRepository = new TurmaRepository();
+
+    PeriodoService periodoService = new PeriodoService(periodoRepository);
+    TurmaService turmaService = new TurmaService(turmaRepository, periodoRepository);
+
+    // Passe os dois serviços para o construtor atualizado da CLI
+    AdminCLI adminCLI = new AdminCLI(periodoService, turmaService);
+
+    
 
     public void iniciar() {
         Scanner scanner = new Scanner(System.in);
---
+++ ClassRoomPB/src/test/java/br/edu/uepb/classroompb/service/PeriodoServiceTest.java
@@ -0,0 +1,43 @@
+// src/test/java/br/edu/uepb/classroompb/service/PeriodoServiceTest.java
+package br.edu.uepb.classroompb.service;
+
+import br.edu.uepb.classroompb.service.exception.ValidacaoException;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.File;
+import static org.junit.Assert.*;
+
+public class PeriodoServiceTest {
+
+    private PeriodoService periodoService;
+
+    @Before
+    public void setUp() {
+        // Apaga o arquivo físico de testes antes de cada execução para garantir isolamento limpo
+        File file = new File("data/periodos.txt");
+        if (file.exists()) {
+            file.delete();
+        }
+        periodoService = new PeriodoService();
+    }
+
+    @Test
+    public void testCadastrarPeriodoPersistidoComSucesso() throws ValidacaoException {
+        periodoService.cadastrarPeriodo("2026.2");
+        assertEquals(1, periodoService.listarPeriodos().size());
+        assertEquals("PLANEJADO", periodoService.listarPeriodos().get(0).getStatus());
+    }
+
+    @Test(expected = ValidacaoException.class)
+    public void testCadastrarPeriodoDuplicadoNoArquivo() throws ValidacaoException {
+        periodoService.cadastrarPeriodo("2026.2");
+        periodoService.cadastrarPeriodo("2026.2"); // Deve lançar exceção de duplicidade
+    }
+
+    @Test
+    public void testAtivarPeriodoPersistido() throws ValidacaoException {
+        periodoService.cadastrarPeriodo("2026.2");
+        periodoService.activarPeriodo("2026.2");
+        assertEquals("INICIADO", periodoService.listarPeriodos().get(0).getStatus());
+    }
+}
